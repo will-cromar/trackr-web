3e8000fa3630ef1369ea7c03feaf29f02217238d
will-cromar <wcromar@knights.ucf.edu>
2017-10-29 12:39:02 -0400

    Implemented very rudimentary notification system.

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ SQLITE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', SQLITE_URI)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
