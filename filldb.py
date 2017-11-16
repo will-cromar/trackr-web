5e742320d9bf4d22b6d8404a43a654361aba30a6
will-cromar <wcromar@knights.ucf.edu>
2017-11-16 14:41:15 -0500

    Made database smaller.

--- filldb.py
+++ filldb.py
@@ -1,5 +1,6 @@
 """Fills DB with content from the sql file."""
 from app import db
+from random import randint
 
 db.drop_all()
 db.create_all()
@@ -7,6 +8,8 @@ db.create_all()
 with open('trackr_mini_dump.sql') as infile:
     s = infile.readlines()
     for row in s:
+        if "schedule" in row and randint(0, 10) == 0: pass
+
         if "NULL" not in row and row.strip() is not "":
             try:
                 db.engine.execute(row)
