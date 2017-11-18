5c6bfe54a598d4e6a96a9a048a5e867cd9b4bc97
will-cromar <wcromar@knights.ucf.edu>
2017-11-18 10:53:52 -0500

    Refactored notifications code.

--- app/notifications.py
+++ app/notifications.py
@@ -1,65 +1,50 @@
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 from app import db, models
-from .recommender import get_closest_recommendation
+from .recommender import get_neighbors
 import random
 import time
 
 
-'''
-    Returns a JSON of all recommendation and schedule notifications
-'''
 def get_notifications():
+    """Returns a JSON of all recommendation and schedule notifications"""
     notification_dict = {}
 
     users = db.session.query(models.User).all()
 
     for user in users:
-        recommendations = __fetch_recommendation_data(user)
+        recommendation = __fetch_recommendation(user)
         reminders = __fetch_schedule_data(user)
-        notification_dict[user.username] = recommendations + reminders
+        notification_dict[user.username] = recommendation + reminders
 
     return notification_dict
 
 
-'''
-    Gathers a list of recommendations (1 per user) and returns a dictionary of that data
-'''
-def __fetch_recommendation_data(user):
-    recommendation_data = []
-
+def __fetch_recommendation(user):
+    """Returns a list containing 0 or 1 recommendations for a user."""
     subs = user.subscriptions
 
     if len(subs) <= 0:
-        return {}
+        return []
 
-    rand_listing = subs[random.randint(0, len(subs) - 1)]
+    # Pick a random subscription to recommend off of
+    source = random.choice(subs)
+    listings = models.Listing.query.all()
+    target_index = listings.index(source)
 
-    username = user.username
-    source_title = rand_listing.title
+    # Pick a random neighbor
+    neighbors = get_neighbors(listings, target_index)
+    recommendation = random.choice(neighbors)
 
-    recommendation = get_closest_recommendation(rand_listing)
-
-    if recommendation is None:
-        return {}
-
-    recommendation_title = recommendation.title
-
-    # message = "Based on your interest in {}, we recommend you try {}!".format(source_title, recommendation_title)
-
-    t_data = {}
-    t_data['listing_id'] = recommendation.listing_id
-    t_data['time'] = int(recommendation.release_date.timestamp())
-    t_data['message'] = "We recommend you try {}!".format(recommendation_title)
-    t_data['submessage'] = "Based on your interest in {}".format(source_title)
-
-    return [t_data]
+    return [{
+        'listing_id': recommendation.listing_id,
+        'time': int(recommendation.release_date.timestamp()),
+        'message': "We recommend you try {}!".format(recommendation.title),
+        'submessage': "Based on your interest in {}".format(source.title)
+    }]
 
 
-'''
-    Gathers a list of Schedule data per user and returns a dictionary of that data
-'''
 def __fetch_schedule_data(user):
-
+    """Gathers a list of Schedule data per user and returns a dictionary of that data"""
     upcoming_schedule = db.session.query(models.Schedule.date, models.Schedule.listing_id, models.Listing.title).\
                         filter(models.Schedule.listing_id.in_([i.listing_id for i in user.subscriptions])).\
                         filter(models.Schedule.listing_id == models.Listing.listing_id).\
